cedac3c23e23cd5340cf019a8c47bebb09e61852
SergeantQuickscoper <donisepic30@gmail.com>
2022-01-21 13:17:10 +0300

    can now type in numbers

--- calculatorGUI.py
+++ calculatorGUI.py
@@ -3,20 +3,23 @@ import tkinter
 root = tkinter.Tk()
 root.title("Simple Calculator")
 
-def buttonAdd():
+def buttonAddNo(number):
+    current = entryField.get()
+    entryField.delete(0, tkinter.END)
+    entryField.insert(0, str(current) + str(number))
     return
 
 entryField = tkinter.Entry(root, width=20, borderwidth = 3)
-button1 = tkinter.Button(root, text="1", padx = 20, pady = 5, command=buttonAdd)
-button2 = tkinter.Button(root, text="2", padx = 20, pady = 5, command=buttonAdd)
-button3 = tkinter.Button(root, text="3", padx = 20, pady = 5,command=buttonAdd)
-button4 = tkinter.Button(root, text="4", padx = 20, pady = 5,command=buttonAdd)
-button5 = tkinter.Button(root, text="5", padx = 20, pady = 5, command=buttonAdd)
-button6 = tkinter.Button(root, text="6", padx = 20, pady = 5, command=buttonAdd)
-button7 = tkinter.Button(root, text="7", padx = 20, pady = 5, command=buttonAdd)
-button8 = tkinter.Button(root, text="8", padx = 20, pady = 5, command=buttonAdd)
-button9 = tkinter.Button(root, text="9", padx = 20, pady = 5, command=buttonAdd)
-button0 = tkinter.Button(root, text="0", padx = 51, pady = 5, command=buttonAdd)
+button1 = tkinter.Button(root, text="1", padx = 20, pady = 5, command=lambda: buttonAddNo(1))
+button2 = tkinter.Button(root, text="2", padx = 20, pady = 5, command=lambda: buttonAddNo(2))
+button3 = tkinter.Button(root, text="3", padx = 20, pady = 5, command=lambda: buttonAddNo(3))
+button4 = tkinter.Button(root, text="4", padx = 20, pady = 5, command=lambda: buttonAddNo(4))
+button5 = tkinter.Button(root, text="5", padx = 20, pady = 5, command=lambda: buttonAddNo(5))
+button6 = tkinter.Button(root, text="6", padx = 20, pady = 5, command=lambda: buttonAddNo(6))
+button7 = tkinter.Button(root, text="7", padx = 20, pady = 5, command=lambda: buttonAddNo(7))
+button8 = tkinter.Button(root, text="8", padx = 20, pady = 5, command=lambda: buttonAddNo(8))
+button9 = tkinter.Button(root, text="9", padx = 20, pady = 5, command=lambda: buttonAddNo(9))
+button0 = tkinter.Button(root, text="0", padx = 51, pady = 5, command=lambda: buttonAddNo(0))
 
 entryField.grid(row=0, column=0, columnspan=3, ipady = 10)
 
